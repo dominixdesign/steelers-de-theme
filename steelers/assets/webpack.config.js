562658af7a4f3d059c731fadfa9ef5b3595dae25
Dominik Sander <dominik.sander@googlemail.com>
2022-07-12 20:17:13 +0200

    setup with contao-theme-framework

---
+++ steelers/assets/webpack.config.js
@@ -0,0 +1,21 @@
+const Encore = require("@symfony/webpack-encore");
+
+Encore.setOutputPath("../public")
+  .setPublicPath("/themes/steelers")
+  .setManifestKeyPrefix("")
+  .addEntry("app", "./js/app.js")
+  .disableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enablePostCssLoader()
+  .addLoader({
+    test: /\.(gif|png|jpe?g|svg)$/i,
+    use: ["image-webpack-loader"],
+  })
+  .copyFiles({
+    from: "./images",
+    to: "images/[path][name].[ext]",
+  });
+
+module.exports = Encore.getWebpackConfig();
