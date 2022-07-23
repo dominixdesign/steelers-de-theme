d3390bc21092aebff1b7f99c279828cb24a27c8a
Dominik Sander <dominik.sander@googlemail.com>
2022-07-23 16:56:59 +0200

    gew

--- src/public/webhook-update.php
+++ src/public/webhook-update.php
@@ -1,6 +1,8 @@
 <?php
 
-file_put_contents('test', var_export($_POST, true));
+$post = file_get_contents('php://input');
+
+file_put_contents('test', var_export($post, true));
 
 //exec('chmod 744 ../src/updateTheme.sh');
 //exec('bash -c "exec nohup setsid ../src/updateTheme.sh > /dev/null 2>&1 &"');
