0304bc6c7cf8e7c096e92baf26137ae33adf82a2
Dominik Sander <dominik.sander@googlemail.com>
2022-07-23 16:45:01 +0200

    add webhook

--- src/public/webhook-update.php
+++ src/public/webhook-update.php
@@ -1,5 +1,7 @@
 <?php
 
-exec('chmod 744 ../src/updateTheme.sh');
-exec('bash -c "exec nohup setsid ../src/updateTheme.sh > /dev/null 2>&1 &"');
+file_put_contents('test', var_export($_POST, true));
+
+//exec('chmod 744 ../src/updateTheme.sh');
+//exec('bash -c "exec nohup setsid ../src/updateTheme.sh > /dev/null 2>&1 &"');
 echo 'OK'; 
