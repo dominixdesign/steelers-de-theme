25824ad0993136d3407b4a286afc6368292c981a
Dominik Sander <dominik.sander@googlemail.com>
2022-07-23 16:59:36 +0200

    hreh

--- src/public/webhook-update.php
+++ src/public/webhook-update.php
@@ -2,7 +2,7 @@
 
 $post = file_get_contents('php://input');
 
-file_put_contents('test', var_export($post, true));
+file_put_contents('test', var_export($_SERVER, true));
 
 //exec('chmod 744 ../src/updateTheme.sh');
 //exec('bash -c "exec nohup setsid ../src/updateTheme.sh > /dev/null 2>&1 &"');
