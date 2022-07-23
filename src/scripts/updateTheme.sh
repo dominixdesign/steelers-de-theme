49599f086e4c31a1f1b19b3c3a6e790d322760ab
Dominik Sander <dominik.sander@googlemail.com>
2022-07-23 21:50:04 +0200

    add symlink generator

--- src/scripts/updateTheme.sh
+++ src/scripts/updateTheme.sh
@@ -3,6 +3,7 @@ vendor/bin/contao-console contao:maintenance-mode enable
 composer dump-autoload
 composer install -o
 rm -rf var/cache/prod
+vendor/bin/contao-console contao:symlinks
 vendor/bin/contao-console cache:clear --no-warmup
 vendor/bin/contao-console cache:warmup
 vendor/bin/contao-console contao:maintenance-mode disable
