b7891de1aa510c8a370c63cbc3024a4df46d9622
Dominik Sander <dominik.sander@googlemail.com>
2022-07-24 22:09:39 +0200

    extend games with urls

--- src/TilastotBundle/Resources/contao/dca/tl_tilastot_client_games.php
+++ src/TilastotBundle/Resources/contao/dca/tl_tilastot_client_games.php
@@ -9,71 +9,57 @@
  * file that was distributed with this source code.
  */
 
-$GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
-(
+$GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array(
     // Config
-    'config'   => array
-    (
+    'config'   => array(
         'dataContainer' => 'Table',
         'enableVersioning' => true,
-        'sql' => array
-        (
-            'keys' => array
-            (
+        'sql' => array(
+            'keys' => array(
                 'id' => 'primary',
-								'hometeam' => 'index',
-								'awayteam' => 'index'
+                'hometeam' => 'index',
+                'awayteam' => 'index'
             )
         )
     ),
     // List
-    'list' => array
-    (
-        'sorting' => array
-        (
+    'list' => array(
+        'sorting' => array(
             'mode'                    => 11,
             'fields'                  => array('gamedate'),
             'panelLayout'             => 'filter;search,limit'
         ),
-        'label' => array
-        (
+        'label' => array(
             'fields'                  => array('gamedate', 'hometeam', 'awayteam', 'round'),
             'showColumns'             => true,
-						'label_callback'					=> array('App\\Tilastot\\Model\\Standings', 'findTeamsForDisplay')
+            'label_callback'                    => array('App\\Tilastot\\Model\\Standings', 'findTeamsForDisplay')
         ),
-        'global_operations' => array
-        (
-            'all' => array
-            (
+        'global_operations' => array(
+            'all' => array(
                 'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
                 'href'                => 'act=select',
                 'class'               => 'header_edit_all',
                 'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
             )
         ),
-        'operations' => array
-        (
-            'edit' => array
-            (
+        'operations' => array(
+            'edit' => array(
                 'label'               => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['edit'],
                 'href'                => 'act=edit',
                 'icon'                => 'edit.gif'
             ),
-            'copy'   => array
-            (
+            'copy'   => array(
                 'label'               => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['copy'],
                 'href'                => 'act=paste&amp;mode=copy',
                 'icon'                => 'copy.gif',
             ),
-            'delete' => array
-            (
+            'delete' => array(
                 'label'               => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
                 'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
             ),
-            'show' => array
-            (
+            'show' => array(
                 'label'               => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['show'],
                 'href'                => 'act=show',
                 'icon'                => 'show.gif'
@@ -81,53 +67,45 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
         )
     ),
     // Palettes
-    'palettes' => array
-    (
+    'palettes' => array(
         'default' => 'round,hometeam,awayteam,gameday,gamedate,gametime,location,spectators,periodscore,homescore,awayscore,resulttype,gamestatus,ended'
     ),
     // Fields
-    'fields'   => array
-    (
-        'id' => array
-        (
-					'sql'                     => "int(10) unsigned NOT NULL AUTO_INCREMENT"
+    'fields'   => array(
+        'id' => array(
+            'sql'                     => "int(10) unsigned NOT NULL AUTO_INCREMENT"
         ),
-        'tstamp' => array
-        (
+        'tstamp' => array(
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
         ),
-        'round' => array
-        (
+        'round' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['round'],
             'exclude'                 => true,
             'filter'                  => true,
             'inputType'               => 'select',
-						'options_callback'				=> array('App\\Tilastot\\Model\\Rounds', 'findForSelect'),
+            'options_callback'                => array('App\\Tilastot\\Model\\Rounds', 'findForSelect'),
             'eval'                    => array('mandatory' => true, 'tl_class' => 'clr w50', 'onchange' => 'Backend.autoSubmit(\'tl_tilastot_client_games\')'),
             'sql'                     => "int(10) unsigned NOT NULL default '0'"
         ),
-        'hometeam' => array
-        (
+        'hometeam' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['hometeam'],
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'select',
-						'options_callback'			  => array('App\\Tilastot\\Model\\Standings', 'findTeamsForSelect'),
+            'options_callback'              => array('App\\Tilastot\\Model\\Standings', 'findTeamsForSelect'),
             'eval'                    => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'clr w50'),
             'sql'                     => "int(10) NULL"
         ),
-        'awayteam' => array
-        (
+        'awayteam' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['awayteam'],
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'select',
-						'options_callback'			  => array('App\\Tilastot\\Model\\Standings', 'findTeamsForSelect'),
+            'options_callback'              => array('App\\Tilastot\\Model\\Standings', 'findTeamsForSelect'),
             'eval'                    => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(10) NULL"
         ),
-        'gameday' => array
-        (
+        'gameday' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['gameday'],
             'exclude'                 => true,
             'search'                  => true,
@@ -135,8 +113,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('rgxp' => 'numeric', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(25) NULL"
         ),
-        'gamedate' => array
-        (
+        'gamedate' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['gamedate'],
             'exclude'                 => true,
             'search'                  => true,
@@ -144,8 +121,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('datepicker' => true, 'rgxp' => 'date', 'mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(10) unsigned NOT NULL"
         ),
-        'gametime' => array
-        (
+        'gametime' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['gametime'],
             'exclude'                 => true,
             'search'                  => true,
@@ -153,8 +129,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "varchar(25) NOT NULL default ''"
         ),
-        'location' => array
-        (
+        'location' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['location'],
             'exclude'                 => true,
             'search'                  => true,
@@ -162,8 +137,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "varchar(255) NOT NULL default ''"
         ),
-        'spectators' => array
-        (
+        'spectators' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['spectators'],
             'exclude'                 => true,
             'search'                  => true,
@@ -171,8 +145,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('rgxp' => 'numeric', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(25) NULL"
         ),
-        'periodscore' => array
-        (
+        'periodscore' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['periodscore'],
             'exclude'                 => true,
             'search'                  => true,
@@ -180,8 +153,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('mandatory' => false, 'maxlength' => 50, 'tl_class' => 'w50'),
             'sql'                     => "varchar(50) NULL"
         ),
-        'homescore' => array
-        (
+        'homescore' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['homescore'],
             'exclude'                 => true,
             'search'                  => true,
@@ -189,8 +161,7 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('rgxp' => 'numeric', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(25) NULL"
         ),
-        'awayscore' => array
-        (
+        'awayscore' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['awayscore'],
             'exclude'                 => true,
             'search'                  => true,
@@ -198,18 +169,16 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('rgxp' => 'numeric', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "int(25) NULL"
         ),
-        'resulttype' => array
-        (
+        'resulttype' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['resulttype'],
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'select',
-						'options'									=> array('' => 'regulär','OT' => 'Overtime','SO' => 'Shootout','OR' => 'Spielwertung'),
+            'options'                                    => array('' => 'regulär', 'OT' => 'Overtime', 'SO' => 'Shootout', 'OR' => 'Spielwertung'),
             'eval'                    => array('rgxp' => 'alpha', 'mandatory' => false, 'maxlength' => 5, 'tl_class' => 'w50'),
             'sql'                     => "varchar(5) NULL"
         ),
-        'gamestatus' => array
-        (
+        'gamestatus' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['gamestatus'],
             'exclude'                 => true,
             'search'                  => true,
@@ -217,14 +186,29 @@ $GLOBALS['TL_DCA']['tl_tilastot_client_games'] = array
             'eval'                    => array('maxlength' => 255, 'tl_class' => 'w50'),
             'sql'                     => "varchar(50) NULL"
         ),
-        'ended' => array
-        (
+        'ended' => array(
             'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['ended'],
             'exclude'                 => true,
             'search'                  => true,
             'inputType'               => 'checkbox',
             'eval'                    => array('tl_class' => 'w50'),
             'sql'                     => "int(1) NOT NULL default '0'"
+        ),
+        'magentaurl' => array(
+            'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['magentaurl'],
+            'exclude'                 => true,
+            'search'                  => true,
+            'inputType'               => 'text',
+            'eval'                    => array('rgxp' => 'url', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
+            'sql'                     => "varchar(255) NULL"
+        ),
+        'eventimurl' => array(
+            'label'                   => &$GLOBALS['TL_LANG']['tl_tilastot_client_games']['eventimurl'],
+            'exclude'                 => true,
+            'search'                  => true,
+            'inputType'               => 'text',
+            'eval'                    => array('rgxp' => 'url', 'mandatory' => false, 'maxlength' => 255, 'tl_class' => 'w50'),
+            'sql'                     => "varchar(255) NULL"
         )
     )
 );
