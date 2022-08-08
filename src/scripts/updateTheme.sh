cfea2fa7bb78d47c649fac7bff0d0ea1bff153ac
Dominik Sander <dominik.sander@googlemail.com>
2022-08-08 12:03:12 +0200

    avoid interaction on db migrations

--- src/scripts/updateTheme.sh
+++ src/scripts/updateTheme.sh
@@ -4,7 +4,7 @@ composer dump-autoload
 composer install -o
 rm -rf var/cache/prod
 vendor/bin/contao-console contao:symlinks
-vendor/bin/contao-console contao:migrate
+vendor/bin/contao-console contao:migrate --no-interaction
 vendor/bin/contao-console cache:clear --no-warmup
 vendor/bin/contao-console cache:warmup
 vendor/bin/contao-console contao:maintenance-mode disable
