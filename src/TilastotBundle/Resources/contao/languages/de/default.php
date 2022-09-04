3a455db1b38b3903c29250c104d80ee6f6c45598
Dominik Sander <dominik.sander@googlemail.com>
2022-09-04 23:21:42 +0200

    start with standings

--- src/TilastotBundle/Resources/contao/languages/de/default.php
+++ src/TilastotBundle/Resources/contao/languages/de/default.php
@@ -12,5 +12,20 @@
 /**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['MSC']['delId'] = array('DEL ID der aktuellen Runde','Trage hier die ID der aktuelle Runde ein.');
+$GLOBALS['TL_LANG']['MSC']['delId'] = array('DEL ID der aktuellen Runde', 'Trage hier die ID der aktuelle Runde ein.');
 $GLOBALS['TL_LANG']['MSC']['tilastot_legend'] = 'DEL Einstellungen';
+
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['name'] = 'Mannschaft';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['shortname'] = 'Team';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['city'] = 'Stadt';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['games'] = 'Spiele';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['rw'] = 'Siege';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['ow'] = 'OT-Siege';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['pw'] = 'Penalty-Siege';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['pl'] = 'Penalty-Niederlagen';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['ol'] = 'OT-Niederlagen';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['rl'] = 'Niederlagen';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['points'] = 'Punkte';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['goalsfor'] = 'Tore';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['goalsagainst'] = 'Gegentore';
+$GLOBALS['TL_LANG']['tilastot_standings_columns']['penalties'] = 'Strafzeiten';
