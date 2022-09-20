9a22e818a13fb2d35267f42a6c83dacda17a5d5e
Dominik Sander <dominik.sander@googlemail.com>
2022-09-20 21:25:14 +0200

    add links to content

--- src/TilastotBundle/Resources/contao/dca/tl_content.php
+++ src/TilastotBundle/Resources/contao/dca/tl_content.php
@@ -3,3 +3,5 @@
 // wrapper
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStart'] = '{type_legend},type;{link_legend},headline;{expert_legend:hide},cssID;{template_legend:hide},customTpl;';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStop'] = '{type_legend},type;{template_legend:hide},customTpl;';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['text'] = '{type_legend},type,headline;{text_legend},text;{link_legend},url,target,linkTitle,embed,titleText,rel;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['fields']['url']['eval']['mandatory'] = false;
