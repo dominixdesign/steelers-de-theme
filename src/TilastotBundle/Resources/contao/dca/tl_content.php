57d98d745724c8ec7140d941bda77dfff115cf97
Dominik Sander <dominik.sander@duerrdental.com>
2023-07-21 15:34:26 +0200

    image for individual template

--- src/TilastotBundle/Resources/contao/dca/tl_content.php
+++ src/TilastotBundle/Resources/contao/dca/tl_content.php
@@ -4,5 +4,5 @@
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStart'] = '{type_legend},type;{link_legend},headline;{expert_legend:hide},cssID;{template_legend:hide},customTpl;';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['rowStop'] = '{type_legend},type;{template_legend:hide},customTpl;';
 $GLOBALS['TL_DCA']['tl_content']['palettes']['text'] = '{type_legend},type,headline;{text_legend},text;{link_legend},url,target,linkTitle,embed,titleText,rel;{image_legend},addImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['template'] = '{type_legend},type,headline;{template_legend},data,customTpl;{imglink_legend:hide},useImage;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['template'] = '{type_legend},type,headline;{template_legend},data,customTpl;{image_legend},addImage;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
 $GLOBALS['TL_DCA']['tl_content']['fields']['url']['eval']['mandatory'] = false;
