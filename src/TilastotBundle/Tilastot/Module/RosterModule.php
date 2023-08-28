66943555e1bd023193e183f39e74d242ab66f93b
Dominik Sander <dominik.sander@duerrdental.com>
2023-08-28 13:42:55 +0200

    adjustments from DEL to DEL2

--- src/TilastotBundle/Tilastot/Module/RosterModule.php
+++ src/TilastotBundle/Tilastot/Module/RosterModule.php
@@ -40,12 +40,19 @@ class RosterModule extends AbstractFrontendModuleController
 			switch ($p['position']) {
 				case 'RW':
 				case 'C':
+				case 'F':
 				case 'CE':
 				case 'LW':
 					$playerlist['FO'][] = $p;
 					break;
+				case 'G':
+					$playerlist['GK'][] = $p;
+					break;
+				case 'D':
+					$playerlist['DE'][] = $p;
+					break;
 				default:
-					$playerlist[$p['position']][] = $p;
+					$playerlist[$p['position']][] = $p; 
 					break;
 			}
 		}
@@ -59,12 +66,12 @@ class RosterModule extends AbstractFrontendModuleController
 		ksort($playerlist);
 
 		$template->players = $playerlist;
-		$template->configData = json_decode(preg_replace('/(\v|\s)+/', ' ', $this->tilastot_config_json), true);
+		$template->configData = json_decode(preg_replace('/(\v|\s)+/', ' ', $model->tilastot_config_json), true);
 
-		$template->headline = $this->headline;
-		$template->headlineUnit = $this->hl;
-		$template->cssId = $this->cssID[0];
-		$template->cssClass = $this->cssID[1];
+		$template->headline = $model->headline;
+		$template->headlineUnit = $model->hl;
+		$template->cssId = $model->cssID[0];
+		$template->cssClass = $model->cssID[1];
 
 		return $template->getResponse();
 	}
