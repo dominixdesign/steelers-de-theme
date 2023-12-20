1650716b4c63738bfd9ab76ef8c23af26c40a8de
Dominik Sander <dominik.sander@duerrdental.com>
2023-12-20 15:15:04 +0100

    make it available for all forms

--- src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
+++ src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
@@ -16,25 +16,23 @@ class LoadFormFieldListener
 {
     public function __invoke(Widget $widget, string $formId, array $formData, Form $form): Widget
     {
-        if ($formId == 'auto_form_12') {
-            if (is_array($widget->options) && $widget->options[0]['value'] == 'gamedays') {
-                $column = array('gamedate >= ? AND hometeam = ?');
-                $games = Games::findAll(array(
-                    'order'   => ' gamedate ASC',
-                    'column'  => $column,
-                    'value'   => array(time(), 36)
-                ));
-                if (!$games) {
-                    $widget->options = array('value' => 'no-game-found', 'label' => "Kein Spiel gefunden.");
-                } else {
-                    $gameArray = $games->fetchAll();
-                    $widget->options = array_map(function ($game) {
-                        $away = Standings::findByIdAndRound($game['awayteam'], $game['round'], true);
-                        $date = \Contao\Date::parse('d.m.Y', $game['gamedate']);
-                        $text = $date . ' - '  . $away['name'];
-                        return array('value' => $text, 'label' => $text);
-                    }, $gameArray);
-                }
+        if (is_array($widget->options) && $widget->options[0]['value'] == 'gamedays') {
+            $column = array('gamedate >= ? AND hometeam = ?');
+            $games = Games::findAll(array(
+                'order'   => ' gamedate ASC',
+                'column'  => $column,
+                'value'   => array(time(), 36)
+            ));
+            if (!$games) {
+                $widget->options = array('value' => 'no-game-found', 'label' => "Kein Spiel gefunden.");
+            } else {
+                $gameArray = $games->fetchAll();
+                $widget->options = array_map(function ($game) {
+                    $away = Standings::findByIdAndRound($game['awayteam'], $game['round'], true);
+                    $date = \Contao\Date::parse('d.m.Y', $game['gamedate']);
+                    $text = $date . ' - '  . $away['name'];
+                    return array('value' => $text, 'label' => $text);
+                }, $gameArray);
             }
         }
         return $widget;
