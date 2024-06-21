5e6a34433bfa3f8987c88d1b2514aadc7a958261
Dominik Sander <dominik.sander@duerrdental.com>
2024-06-21 10:15:14 +0200

    troubleshooting

--- src/TilastotBundle/Tilastot/Controller/SeasonTicketController.php
+++ src/TilastotBundle/Tilastot/Controller/SeasonTicketController.php
@@ -4,14 +4,12 @@ namespace App\Tilastot\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/seasontickets/order', name: SeasonTicketController::class)]
-#[AsController]
 class SeasonTicketController
 {
-  public function __invoke(Request $request): Response
+  #[Route('/seasontickets/order', name: SeasonTicketController::class)]
+  public function order(Request $request): Response
   {
     $data = json_decode($request->getContent(), true);  
     var_dump($data);
