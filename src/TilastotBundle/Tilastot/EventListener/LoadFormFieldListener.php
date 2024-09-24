0c9a542ab5b2b8b3fb21f80746a9a538093244f6
Dominik Sander <dominik.sander@duerrdental.com>
2024-09-24 12:47:05 +0200

    Remove games from selection that started in the next 7 hours

--- src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
+++ src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
@@ -21,7 +21,7 @@ class LoadFormFieldListener
             $games = Games::findAll(array(
                 'order'   => ' gamedate ASC',
                 'column'  => $column,
-                'value'   => array(time(), 54744)
+                'value'   => array(time() + (7 * 60 * 60), 54744)
             ));
             if (!$games) {
                 $widget->options = array('value' => 'no-game-found', 'label' => "Kein Spiel gefunden.");
