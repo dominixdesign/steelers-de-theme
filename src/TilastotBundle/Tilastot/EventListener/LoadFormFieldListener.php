865b9e1a3c4c3a53182bdd8771e0093963b2736d
Dominik Sander <dominik.sander@duerrdental.com>
2024-09-25 15:32:51 +0200

    Refactor LoadFormFieldListener to add support for porsche camps

--- src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
+++ src/TilastotBundle/Tilastot/EventListener/LoadFormFieldListener.php
@@ -8,6 +8,7 @@ use Contao\Widget;
 
 use App\Tilastot\Model\Games;
 use App\Tilastot\Model\Standings;
+use App\Tilastot\Model\Camps;
 
 /**
  * @Hook("loadFormField")
@@ -33,6 +34,31 @@ class LoadFormFieldListener
                     $text = $date . ' - '  . $away['name'];
                     return array('value' => $text, 'label' => $text);
                 }, $gameArray);
+            }
+        } else if (is_array($widget->options) && $widget->options[0]['value'] == 'porschecamps') {
+            $column = array('gamedate >= ? AND hometeam = ?');
+            $camps = Camps::findAll(array(
+                    'order'   => ' startdate ASC',
+                    'column'  => array('published=?', 'startdate>?'),
+                    'value'   => array(1, time())
+                ));
+            if (!$camps) {
+                $widget->options = array('value' => 'no-camp-found', 'label' => "Kein Camps geplant.");
+            } else {
+                $campsArray = $camps->fetchAll();
+                $widget->options = array_map(function ($camp) {
+                    $timezone = new \DateTimeZone('Europe/Berlin');
+                    $timestamp1 = $camp['startdate'];
+                    $timestamp2 = $camp['enddate'];
+                    $date1 = new \DateTime("@$timestamp1");
+                    $date1->setTimezone($timezone);
+                    $date2 = new \DateTime("@$timestamp2");
+                    $date2->setTimezone($timezone);
+                    $formattedDate1 = $date1->format('d');
+                    $formattedDate2 = $date2->format('d.m.Y');
+                    $text = $formattedDate1 . '. bis ' . $formattedDate2;
+                    return array('value' => $camp['name'], 'label' => $text);
+                }, $campsArray);
             }
         }
         return $widget;
